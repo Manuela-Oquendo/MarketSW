ab1f31db772310c9e47eee991aa389814a8ccfeb
Manuela Franco <manuelafranco300953@correo.itm.edu.co>
2023-09-25 19:16:35 -0500

    Se crea el metodo actualizar y eliminar, se prueba tambien en Swagger

--- Market/Market.API/Controllers/CountriesController.cs
+++ Market/Market.API/Controllers/CountriesController.cs
@@ -1,4 +1,5 @@
 ﻿using Market.API.Data;
+using Market.Shared.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,7 +7,7 @@ namespace Market.API.Controllers
 {
     [ApiController]
     [Route("/api/countries")]
-    public class CountriesController:ControllerBase
+    public class CountriesController : ControllerBase
     {
         private readonly DataContext _context;
 
@@ -14,6 +15,8 @@ namespace Market.API.Controllers
         {
             _context = context;
         }
+
+        //Https protocolo se seguridad de las paginas
 
 
         //Lista de paises
@@ -28,15 +31,64 @@ namespace Market.API.Controllers
         [HttpGet("{id:int}")]
         public async Task<ActionResult> Get(int id)
         {
-            var country = await _context.Countries.FirstOrDefaultAsync(c  => c.Id == id);
+            var country = await _context.Countries.FirstOrDefaultAsync(c => c.Id == id);
 
-            if(country == null)
+            if (country == null)
             {
-               return NotFound();
+                return NotFound(); //404
 
-                
-            } return Ok(country);   
+
+            }
+            return Ok(country);   //200
         }
+
+
+
+
+        //insertar un registro, Post -> insert
+        [HttpPost]
+        public async Task<ActionResult> Post(Country country)
+        {
+            _context.Add(country); //agrega un nuevo pais
+            await _context.SaveChangesAsync(); //esta intruccion Save... me salva o me guarda lo que se ingrese
+            return Ok(country); //200 si guarda el pais seria un 200, muestra un resumen de lo que se guardo
+        }
+
+
+
+
+        //actualizacion de tablas
+        [HttpPut]
+        public async Task<ActionResult> Put(Country country)
+        {
+            _context.Update(country); //aqui ya no estamos agregando sino actualizando los registros
+            await _context.SaveChangesAsync(); //guarda los cambios en la tabla
+            return Ok(country); //200 -> muestra un resumen de lo que se actualizo
+        }
+
+
+
+        //delete -> borrado de datos
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var filaafectada = await _context.Countries
+                .Where(c => c.Id == id)
+                .ExecuteDeleteAsync();
+
+            if(filaafectada == 0) //aqui se le esta preguntando si no existe el registro
+            {
+
+                return NotFound(); //404
+
+
+            }
+            return NoContent();//204 -> borre pero no me muestre que borro, NO MUESTRA UN RESUMEN, ningun contenido
+
+        }
+
+
+
 
     }
 }
